f9f9913656801b72b2c5b6518a2e7c599e995b29
markchck <markchck@gmail.com>
2023-08-21 15:13:22 +0900

    temp - 라우팅시 status 값 landing, login, register, main으로 변경 중

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue"
 import App from "./App.vue"
 import router from "./router"
 import store from "./store"
-import vuetify from './plugins/vuetify'
+import vuetify from "./plugins/vuetify"
 
 Vue.config.productionTip = false
 
@@ -10,5 +10,8 @@ new Vue({
   store,
   router,
   vuetify,
-  render: (h) => h(App)
+  // beforeCreate() {
+  //   this.$store.dispatch("getMemberInfo")
+  // },
+  render: (h) => h(App),
 }).$mount("#app")
--- src/router/index.js
+++ src/router/index.js
@@ -2,45 +2,65 @@ import Vue from "vue"
 import VueRouter from "vue-router"
 import Login from "../views/Login.vue"
 import Register from "../views/Register.vue"
-import store from "../store.js"
+// import store from "../store.js"
 import Main from "../views/Main.vue"
 Vue.use(VueRouter)
 
 const rejectAuthUser = (to, from, next) => {
-  if (store.state.accessToken) {
+  if (localStorage.getItem("accessToken")) {
     //이미 로그인 된 유저니까 막아함.
-    alert("이미로그인")
-    next("/") // next에 경로를 적으면 리다이렉션
+    alert("이미 로그인한 상태입니다.")
+    next("/main")
   } else {
-    next() // 경로가 없으면 componet를 실행(입장을 허용)
+    next()
   }
 }
 const onlyAuthUser = (to, from, next) => {
-  if (!store.state.accessToken) {
+  if (!localStorage.getItem("accessToken")) {
     //로그인 안 된 유저니까 막아함.
+
     alert("로그인하고 오셈")
-    next("/login") // next에 경로를 적으면 리다이렉션
+    next("/login")
   } else {
-    next() // 경로가 없으면 componet를 실행(입장을 허용)
+    next()
   }
 }
+// const changeSatatus = (to, from, next, status) => {
+//   alert("들어왔다.")
+//   store.commit("setStatus", status)
+//   console.log(store.state.status)
+//   next()
+// }
 
 const routes = [
   {
     path: "/login",
     name: "Login",
-    beforeEnter: rejectAuthUser,
+    beforeEnter: [
+      rejectAuthUser,
+      // (to, from, next) => changeSatatus(next, "login"),
+      // alert(store.state.status),
+    ],
     component: Login,
   },
   {
     path: "/register",
     name: "Register",
+    beforeEnter: [
+      rejectAuthUser,
+      // (to, from, next) => changeSatatus(next, "register"),
+      // alert(store.state.status),
+    ],
     component: Register,
   },
   {
     path: "/main",
     name: "Main",
-    beforeEnter: onlyAuthUser,
+    beforeEnter: [
+      onlyAuthUser,
+      // (to, from, next) => changeSatatus(next, "main"),
+      // alert(store.state.status),
+    ],
     component: Main,
   },
 ]
